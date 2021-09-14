087e3f78fe6f660795aa0242a6a896e50e433930
niharika-1999 <niharika.rao@ymedialabs.com>
2021-09-14 23:10:01 +0530

    Added:Password Rule 2 to check to have atleast 1 uppercase

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 	public final String LAST_NAME ="^[A-Z]{1}[a-z]{2,}$";
 	public final String EMAIL = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
 	public final String PHONE = "^[0-9]{2}\s?[0-9]{10}$";
-	public final String PASSWORD = "^.{8,}";
+	public final String PASSWORD = "^(?=.*[A-Z]).{8,}$";
 	
 	public boolean checkFirstName(String first_name) //Method to check first name is valid or not
 	{
