c1a9b3820e183cbeabf6f4b7f05ae01b57e9d2ea
niharika-1999 <88999824+niharika-1999@users.noreply.github.com>
2021-09-14 23:27:15 +0530

    Update UserRegTest.java

--- src/test/java/com/bridgelabz/UserRegTest.java
+++ src/test/java/com/bridgelabz/UserRegTest.java
@@ -73,15 +73,15 @@ public class UserRegTest {
 	public void PasswordReturnTrueWhenValid()
 	{
 		UserRegistration obj = new UserRegistration();
-		boolean result=obj.checkPhone("Password@123");
-		assertEquals(false,result);
+		boolean result=obj.checkPassword("Password@123");
+		assertEquals(true,result);
 	}
 	
 	@Test
 	public void PasswordReturnFalseWhenInValid()
 	{
 		UserRegistration obj = new UserRegistration();
-		boolean result=obj.checkPhone("Pass");
+		boolean result=obj.checkPassword("Pass");
 		assertEquals(false,result);
 	}
 	
